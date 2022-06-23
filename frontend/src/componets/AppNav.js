4e220b8abb376a62abcc04a43fe1e45819225d68
edisson11osorio <osorioedisson08@gmail.com>
2022-06-23 02:07:33 -0500

    [🐛] fix a bug

--- frontend/src/componets/AppNav.js
+++ frontend/src/componets/AppNav.js
@@ -1,25 +1,33 @@
-import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Logo from "../images/logo.png"
-
-
-import "./styles/AppNav.css"
+import User from "../images/user.png"
+import Button from '@material-ui/core/Button';
+import "../styles/AppNav.css"
 import styles from "./styles/Nav.styles.js"
 
 
-class AppNav extends Component {
-    render() {
-        const clases = styles();
-        return (
-            <div>
-                <AppBar position="fixed"  className={clases.NavApp}>
-                    <Toolbar>
-                        <img src={Logo} className="logo" />
-                    </Toolbar>
-                </AppBar>
-            </div>
-        )
-    }
+function AppNav() {
+    const clases = styles();
+    return (
+        <div>
+            <AppBar position="fixed" className={clases.Nav}>
+                <img src={Logo} className="logo" />
+                <img src={User} className="User" />
+                <Toolbar>
+                    <div className={clases.boton1}>
+                        <Button >
+                            About us
+                        </Button>
+                    </div>
+                    <div className={clases.boton2}>
+                        <Button >
+                            Foundations
+                        </Button>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 }
 export default AppNav
