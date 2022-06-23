0e1872df08433a5328286135b825b4badfaeb141
edisson11osorio <osorioedisson08@gmail.com>
2022-06-23 10:38:12 -0500

    [🚧] work in progress

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 // import AppNav from "./componets/AppNav";
 // import Footer from "./componets/Footer";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
+import App from "./pages/App";
 
 
 
 const rootElement = document.getElementById("root")
 
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<Login/>)
+root.render(<App/>)
--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import AppNav from "../componets/AppNav";
 // import Footer from "../componets/Footer";
+import "../styles/App.css"
+import Tijano from "../images/tijano.png"
 
-function App () {
+function App() {
     return (
-       <AppNav></AppNav>
-
+        <div>
+            <AppNav></AppNav>
+            <section className="contend">
+                <img src={Tijano} className="tijano"></img>
+            </section>
+            <div>
+                <h1 className="titulo">Colombian art, created <br></br>by mothers who are<br></br>heads of households</h1>
+            </div>
+        </div>
     )
 }
-
 export default App
