3ce6bb0339b0960de8d6610814a208b91ccd877e
edisson11osorio <osorioedisson08@gmail.com>
2022-06-23 21:13:59 -0500

    [💄] adding styles for template Foundations material-ui

---
+++ frontend/src/pages/styles/Foundations.styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles({
+         BTN: {
+            backgroundColor: 'green',
+            color: 'white',
+            position: "relative",
+            left: '14%',
+            borderRadius: '20px',
+            marginTop: '13px'
+         }
+});
