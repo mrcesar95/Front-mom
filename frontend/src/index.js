00ae24a618d7dea77e8156c7f257e6838a7bd750
edisson11osorio <osorioedisson08@gmail.com>
2022-06-23 21:27:08 -0500

    fix code and update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,10 @@ import * as ReactDOMClient from "react-dom/client";
 // import AppNav from "./componets/AppNav";
 // import Footer from "./componets/Footer";
 // import Login from "./pages/Login";
-import Foundations from "./pages/Foundations";
-
+// import Foundations from "./pages/Foundations";
+import Product from "./pages/Product";
 
 const rootElement = document.getElementById("root")
 
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<Foundations/>)
+root.render(<Product/>)
