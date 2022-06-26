c950943aea884f3125073fd6943cd2b9cb1bef66
edisson11osorio <osorioedisson08@gmail.com>
2022-06-26 12:31:43 -0500

    [✅]add images for slider

---
+++ src/exports/Images.js
@@ -0,0 +1,8 @@
+import img1 from "../slider/fique.png"
+import img2 from "../slider/frutero.png"
+import img3 from "../slider/tarro.jpg"
+import img4 from "../slider/botes.png"
+import img5 from "../slider/base.gif"
+import img6 from "../slider/portalapiz.gif"
+
+export default [img1, img2, img3, img4, img5, img6] 
