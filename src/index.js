978bb47198b7a9c1ff6f61d4def8d140630ac162
edisson11osorio <osorioedisson08@gmail.com>
2022-06-26 12:28:12 -0500

    [✅] new images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import About from "./pages/AboutUs";
+import App from "./componets/App";
+import { BrowserRouter } from 'react-router-dom';
+// import About from "./pages/AboutUs";
 // import App from "./pages/App";
 // import Foundations from "./pages/Foundations"
 // import About from "./pages/AboutUs";
-import App from "./pages/App";
+// import Api from "./pages/Api";
 
 
 const rootElement = document.getElementById("root")
 
 const root = ReactDOMClient.createRoot(rootElement);
-root.render(<About/>)
+root.render(
+    <BrowserRouter>
+       <App />
+</BrowserRouter>,
+)
