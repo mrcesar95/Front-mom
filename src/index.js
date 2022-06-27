19e26837dea604d965eb223df840b01e6d927a20
edisson11osorio <osorioedisson08@gmail.com>
2022-06-27 09:10:21 -0500

    [✅]update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./componets/App";
 import { BrowserRouter } from 'react-router-dom';
-// import About from "./pages/AboutUs";
-// import App from "./pages/App";
-// import Foundations from "./pages/Foundations"
-// import About from "./pages/AboutUs";
-// import Api from "./pages/Api";
 
 
 const rootElement = document.getElementById("root")
@@ -14,7 +9,6 @@ const rootElement = document.getElementById("root")
 const root = ReactDOMClient.createRoot(rootElement);
 root.render(
     <BrowserRouter>
-    <AppRegister />
        <App />
 </BrowserRouter>,
 )
