3df2e5d4b909175342eae10a68a585d090531dad
edisson11osorio <osorioedisson08@gmail.com>
2022-07-04 16:10:01 -0500

    [✅]update code

---
+++ src/componets/Redes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Git from "../images/git.png"
+import Lin from "../images/linke.png"
+
+function Redes(){
+    return(
+        <img src={Git} className="GIT1"/>
+    )
+}
+export default Redes
