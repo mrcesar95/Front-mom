de4309b0a70d47881870cd41724b3d1d1f4afde3
Cesar Molina <cesaruxfxtx@gmail.com>
2022-07-05 09:58:36 -0500

    Adding heroku app

--- src/pages/services/constants.js
+++ src/pages/services/constants.js
@@ -1,2 +1,2 @@
-
-export const baseUrl = 'https://62bcf1b86b1401736c02a67f.mockapi.io/api/v1/login';
+//export const baseUrl = 'https://62bcf1b86b1401736c02a67f.mockapi.io/api/v1/login';
+export const baseUrl = 'https://tim-eds-81456.herokuapp.com/';
