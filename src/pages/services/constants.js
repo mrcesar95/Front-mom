1b05bcee058f2eb49cb574d56a7e5c599af12b4c
joacasallas <joacasallas@gmail.com>
2022-07-06 10:04:31 -0500

    update

--- src/pages/services/constants.js
+++ src/pages/services/constants.js
@@ -1,2 +1,3 @@
 //export const baseUrl = 'https://62bcf1b86b1401736c02a67f.mockapi.io/api/v1/login';
-export const baseUrl = 'https://tim-eds-81456.herokuapp.com/';
+//export const baseUrl = 'https://tim-eds-81456.herokuapp.com/';
+export const baseUrl = 'https://serene-olympic-17983.herokuapp.com/api/notes';
