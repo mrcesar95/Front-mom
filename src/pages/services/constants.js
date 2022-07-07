2022868c213b1dd14b890eb0f504c161978ae5f1
joacasallas <joacasallas@gmail.com>
2022-07-07 15:22:10 -0500

    update

--- src/pages/services/constants.js
+++ src/pages/services/constants.js
@@ -1,3 +1,1 @@
-//export const baseUrl = 'https://62bcf1b86b1401736c02a67f.mockapi.io/api/v1/login';
-//export const baseUrl = 'https://tim-eds-81456.herokuapp.com/';
-export const baseUrl = 'https://pure-hollows-96693.herokuapp.com/api';
+export const baseUrl = 'https://shielded-badlands-86656.herokuapp.com/api';
